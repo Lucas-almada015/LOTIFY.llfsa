ee2385067cd33c1431b190a72948e82965efbacd
lucasAlmada <almadalucas015@gmail.com>
2024-06-04 20:13:02 -0300

    Initial commit

---
+++ app/src/main/java/com/llfsa/lotifyllfsa/AuthActivity.kt
@@ -0,0 +1,29 @@
+package com.llfsa.lotifyllfsa
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import javax.inject.Singleton
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_auth)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+    }
+    //setup
+
+    private fun setup() {
+        title= "login"
+
+
+    }
+}
