8548420ba8d40882d1156d1849396971ae993c40
lucasAlmada <almadalucas015@gmail.com>
2024-06-04 22:11:05 -0300

    intento de arreglo

--- app/src/main/java/com/llfsa/lotifyllfsa/AuthActivity.kt
+++ app/src/main/java/com/llfsa/lotifyllfsa/AuthActivity.kt
@@ -16,7 +16,6 @@ class AuthActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
     }
     //setup
