7ee854472783c9946f4fab6b9bb3e3cfb352245d
lucasAlmada <almadalucas015@gmail.com>
2024-07-02 23:35:49 -0300

    pantalla perfil creada

--- app/src/main/java/com/llfsa/lotifyllfsa/PerfilActivity.kt
+++ app/src/main/java/com/llfsa/lotifyllfsa/PerfilActivity.kt
@@ -1,7 +1,7 @@
 package com.llfsa.lotifyllfsa
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -10,19 +10,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.FirebaseAuth
 
-enum class ProviderType {
-    BASIC
-}
-
-class HomeActivity : AppCompatActivity() {
+class PerfilActivity : AppCompatActivity() {
     private lateinit var emailTextView: TextView
     private lateinit var providerTextView: TextView
-    private lateinit var logoutButton: Button
+    private lateinit var volverButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_perfil)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -30,7 +26,9 @@ class HomeActivity : AppCompatActivity() {
         }
 
         // Inicializar vistas
-        logoutButton = findViewById(R.id.logoutButton)
+        emailTextView = findViewById(R.id.emailTextView)
+        providerTextView = findViewById(R.id.provideTextView)
+        volverButton = findViewById(R.id.button6)
 
         // Setup
         val bundle: Bundle? = intent.extras
@@ -40,13 +38,15 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun setup(email: String, provider: String) {
-        title = "Home"
+        title = "Perfil"
         emailTextView.text = email
         providerTextView.text = provider
-        logoutButton.setOnClickListener {
+        volverButton.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
-            onBackPressed()
+            val intent = Intent(this, HomeActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            finish()
         }
     }
 }
-
